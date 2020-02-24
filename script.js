5c00d58890f96f4a717eaab7d59909e2b0b9326b
Darren Cedrone <dcedrone@Darrens-Air.home>
2020-02-24 08:59:20 -0500

    Added the index, javascript, and css for the web calculator

---
+++ script.js
@@ -0,0 +1,6 @@
+function getSum() {
+    var numberA = document.getElementById("numberA").value
+    var numberB = document.getElementById("numberB").value
+    alert(Number(numberA) + Number(numberB))
+}
+//getSum();
