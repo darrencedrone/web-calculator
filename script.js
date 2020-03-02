87c738786d326a76a65fb7e5e8332011b19ed8ff
Darren Cedrone <dcedrone@Darrens-Air.home>
2020-03-02 00:00:40 -0500

    Added subtraction, multiplication, and division! Also made things even funkier.

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function getSum() {
+function getResult() {
+    var typeOfMath = document.getElementById("typeOfMath").value
     var numberA = document.getElementById("numberA").value
     var numberB = document.getElementById("numberB").value
-    alert(Number(numberA) + Number(numberB))
+
+    if (typeOfMath === 'addition') {
+        alert(Number(numberA) + Number(numberB))
+    } else if (typeOfMath === 'subtraction') {
+        alert(numberA - numberB)
+    } else if (typeOfMath === 'multiplication') {
+        alert(numberA * numberB)
+    } else if (typeOfMath === 'division') {
+        alert(numberA / numberB)
+    } else {
+        alert("Error. Result is not certified funky.")
+    }
 }
-//getSum();
